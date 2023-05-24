078f8018514415b4c5d56f47f86498b862a41763
Wellington de Oliveira <wellington220100@gmail.com>
2023-05-23 21:10:31 -0300

    Alteração

--- paint/ContentView.swift
+++ paint/ContentView.swift
@@ -24,38 +24,52 @@ struct ContentView: View {
     @State private var isDrawing: Bool = false
     @State private var selectedColor: Color = .black
     @State private var widthPen: CGFloat = 2
+    @State private var position: CGPoint = .zero
     
     var colors1: [Color] = [.red, .blue, .green, .gray, .brown]
     var colors2: [Color] = [.yellow, .black, .purple, .cyan]
     
     
     var body: some View {
-        VStack {
-            HStack {
-                ForEach(colors1, id: \.self) { color in
-                    Circle()
-                        .frame(height: 50)
-                        .foregroundColor(color)
-                        .onTapGesture {
-                            selectedColor = color
-                        }
+            VStack {
+                HStack {
+                    ForEach(colors1, id: \.self) { color in
+                        Circle()
+                            .frame(height: 50)
+                            .foregroundColor(color)
+                            .onTapGesture {
+                                selectedColor = color
+                            }
+                    }
                 }
-            }
-            HStack {
-                ForEach(colors2, id: \.self) { color in
-                    Circle()
-                        .frame(height: 50)
-                        .foregroundColor(color)
-                        .onTapGesture {
-                            selectedColor = color
-                        }
+                HStack {
+                    ForEach(colors2, id: \.self) { color in
+                        Circle()
+                            .frame(height: 50)
+                            .foregroundColor(color)
+                            .onTapGesture {
+                                selectedColor = color
+                            }
+                    }
                 }
-            }
-            GeometryReader { geometry in
-                ZStack {
-                    ForEach(strokes) { stroke in
+                GeometryReader { geometry in
+                    ZStack {
+                        ForEach(strokes) { stroke in
+                            Path { path in
+                                for (index, point) in stroke.points.enumerated() {
+                                    if index == 0 {
+                                        path.move(to: point)
+                                    } else {
+                                        path.addLine(to: point)
+                                    }
+                                }
+                            }
+                            .stroke(style: StrokeStyle(lineWidth: stroke.wdPen))
+                            .foregroundColor(stroke.color)
+                        }
+                        
                         Path { path in
-                            for (index, point) in stroke.points.enumerated() {
+                            for (index, point) in currentStroke.enumerated() {
                                 if index == 0 {
                                     path.move(to: point)
                                 } else {
@@ -63,65 +77,64 @@ struct ContentView: View {
                                 }
                             }
                         }
-                        .stroke(style: StrokeStyle(lineWidth: stroke.wdPen))
-                        .foregroundColor(stroke.color)
+                        .stroke(style: StrokeStyle(lineWidth: widthPen))
+                        .foregroundColor(selectedColor)
+                            Circle()
+                                .frame(height: 3*widthPen)
+                                .foregroundColor(selectedColor)
+                                .offset(x: position.x - 180, y: position.y - 280)
+                        
                     }
-                    
-                    Path { path in
-                        for (index, point) in currentStroke.enumerated() {
-                            if index == 0 {
-                                path.move(to: point)
-                            } else {
-                                path.addLine(to: point)
+                    .background(Color.white)
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { value in
+                                let location = value.location
+                                if !isDrawing {
+                                    currentStroke = [location]
+                                    isDrawing = true
+                                } else {
+                                    currentStroke.append(location)
+                                }
+                                position = location
+                            }
+                            .onEnded { value in
+                                isDrawing = false
+                                let stroke = Stroke(points: currentStroke, color: selectedColor, wdPen: widthPen)
+                                strokes.append(stroke)
+                                currentStroke = []
+                            }
+                    )
+                }
+                HStack{
+                    Circle().frame(height: 50).overlay(Image(systemName: "eraser.fill").font(.system(size: 20)).foregroundColor(.white)).onTapGesture {
+                        strokes = []
+                    }
+                    Circle()
+                        .frame(height: 50)
+                        .overlay(Image(systemName: "pencil.tip.crop.circle.badge.plus")
+                            .font(.system(size: 30))
+                            .foregroundColor(.white))
+                        .onTapGesture {
+                            if(!(widthPen >= 10)){
+                                widthPen+=1
                             }
                         }
-                    }
-                    .stroke(style: StrokeStyle(lineWidth: widthPen))
-                    .foregroundColor(selectedColor)
-                }
-                .background(Color.white)
-                .gesture(
-                    DragGesture(minimumDistance: 0)
-                        .onChanged { value in
-                            let location = value.location
-                            if !isDrawing {
-                                currentStroke = [location]
-                                isDrawing = true
-                            } else {
-                                currentStroke.append(location)
+                    Circle()
+                        .frame(height: 50)
+                        .overlay(Image(systemName: "pencil.tip.crop.circle.badge.minus")
+                            .font(.system(size: 30))
+                            .foregroundColor(.white))
+                        .onTapGesture {
+                            if(!(widthPen <= 1)){
+                                widthPen-=1
                             }
                         }
-                        .onEnded { value in
-                            isDrawing = false
-                            let stroke = Stroke(points: currentStroke, color: selectedColor, wdPen: widthPen)
-                            strokes.append(stroke)
-                            currentStroke = []
-                        }
-                )
-            }
-            HStack{
-                Circle().frame(height: 50).overlay(Image(systemName: "eraser.fill").font(.system(size: 20)).foregroundColor(.white)).onTapGesture {
-                    strokes = []
                 }
-                Circle()
-                    .frame(height: 50)
-                    .overlay(Image(systemName: "pencil.tip.crop.circle.badge.plus")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white))
-                    .onTapGesture {
-                        widthPen+=1
-                    }
-                Circle()
-                    .frame(height: 50)
-                    .overlay(Image(systemName: "pencil.tip.crop.circle.badge.minus")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white))
-                    .onTapGesture {
-                        widthPen-=1
-                    }
             }
         }
-    }
+        
+    
     
 }
 
